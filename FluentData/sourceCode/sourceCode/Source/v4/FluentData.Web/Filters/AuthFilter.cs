659ba751bc698640ef377a3db8000250fd53da6a
shuaiagain <764258872@qq.com>
2018-08-10 13:07:52 +0800

    1.添加登录模板

---
+++ FluentData/sourceCode/sourceCode/Source/v4/FluentData.Web/Filters/AuthFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+
+namespace FluentData.Web.Filters
+{
+    public class AuthFilter : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+        }
+    }
+}
